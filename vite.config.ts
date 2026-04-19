d04463777648c9e2e56fb3c9cd66a5f28d4182f5
sovortey <126035238+sovortey514@users.noreply.github.com>
2026-04-19 17:29:00 +0700

    uodate new

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,6 @@ export default defineConfig(({mode}) => {
   return {
     plugins: [react(), tailwindcss()],
     base: "/Sorvortey/",
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
